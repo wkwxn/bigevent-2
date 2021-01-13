a9dd814c7b97f82a8662823921228ccf395473cd
wk <742751416@qq.com>
2021-01-13 17:19:09 +0800

    登录注册页面完成

--- assets/js/ajaxBase.js
+++ assets/js/ajaxBase.js
@@ -1,26 +1,14 @@
 /*
  * @Author: your name
- * @Date: 2021-01-05 10:33:03
- * @LastEditTime: 2021-01-06 16:48:27
+ * @Date: 2021-01-10 19:53:48
+ * @LastEditTime: 2021-01-13 16:43:53
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \bigevent\assets\js\ajaxBase.js
+ * @FilePath: \bigevent-2\assets\js\ajaxBase.js
  */
 $.ajaxPrefilter(function(options){
-    // console.log(options);
-    options.url= 'http://api-breakingnews-web.itheima.net'+options.url
+    // 处理路径
+    options.url='http://api-breakingnews-web.itheima.net'+options.url
 
-// 判断 需要身份认证信息的时候 ，地址携带‘/my’的时候
-    if (options.url.indexOf('/my') !==-1) {
-        options.headers={
-            Authorization:localStorage.getItem('token')
-        }
-    }
-    // 限制用户访问权限
-    options.complete=function(xhr){
-        if (xhr.responseJSON.status===1&&xhr.responseJSON.message==='身份认证失败！') {
-            location.href='/home/login.html' 
-        }
-    }
-
+   
 })
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,103 +1,108 @@
 /*
  * @Author: your name
- * @Date: 2021-01-03 16:29:23
- * @LastEditTime: 2021-01-05 22:11:44
+ * @Date: 2021-01-10 19:14:40
+ * @LastEditTime: 2021-01-13 17:18:05
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \bigevent\assets\js\login.js
+ * @FilePath: \bigevent-2\assets\js\login.js
  */
-$(function(){
 
-    // 点击去注册
-    $('#goregister').on('click',function(){
-        $('.login').hide()
-        $('.register').show()
-    })
-    // 点击去登录
-    $('#gologin').on('click',function(){
-        $('.login').show()
-        $('.register').hide()
-    })
+let form=layui.form
+let layer=layui.layer
+//=========================点击切换登录/注册=========================
+$('#logBtn').click(function(){
+    // console.log(111);
+    $('.register').show()
+    $('.login').hide()
+    // 原生的
+    // document.querySelector('.register').style.display='block'
+    // document.querySelector('.login').style.display='none'
+})
+$('#regBtn').click(function(){
+    $('.register').hide()
+    $('.login').show()
+})
 
-
-// =========================表单验证=========================
-    let form=layui.form;
-
-    // 自定义验证规则
-    form.verify({
-        //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
-        pass: [/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格'] ,
-
-        // 验证注册页面两次密码输入
-        // 需要获取注册表单的第二个密码框
-        respwd: function(value,item){ //value：表单的值、item：表单的DOM对象
-        // console.log(value);
-        // console.log(item);
-        
-        let pwd=  $('.register [name=password]').val();
-        //   console.log(pwd);
-            if (value !== pwd) {
-            // console.log(111);
-                 return '密码输入不一致，请重新输入';
-            }
-        }
-    })  
-
-// =========================实现注册功能=========================
-let layer = layui.layer;
-$('#regisform').on('submit',function(e){
-    // 1.阻止默认行为
-    // 2.获取data数据
-    // 3.发送ajax请求
-    // 4.弹出提示信息
+//=========================登录功能=========================
+$('#loginform').on('submit',function(e){
     e.preventDefault()
+    // console.log(111);
     let data= $(this).serialize()
-    // console.log(data);
+ console.log(data);
+
     $.ajax({
         type:'POST',
-        url:'/api/reguser',
+        url:'/api/login',
         data,
         success:function(res){
-            // console.log(res);
-            if (res.status !== 0) {
-                return layer.msg(res.message);
-            }else{  
-                layer.msg('注册成功，即将跳转到登录页面', function(){
-                    //do something
-                    $('#gologin').click()
-                  });                
-            }   
+            console.log(res);
+            if (res.status !==0) {
+                return layer.msg('账号密码错误,请重新输入！')
+            }
+            layer.msg('登录成功！',function(){
+                location.href='/home/index.html'
+            })
         }
     })
-  })
-
-
-// =========================实现登录功能=========================
-    $('#loginform').on('submit',function(e){
-    // 1.
-        e.preventDefault()
-
-        let data=$(this).serialize()
-         // console.log(data);
-        // 发送ajax请求
-        $.ajax({
-            type:'POST',
-            url:'/api/login',
-            data,
-            success:function(res){
-                // console.log(res);
-                if (res.status !==0) {
-                    return layer.msg('账号密码错误，请重新输入');
-                }
-                
-                localStorage.setItem('token',res.token)
-
-                layer.msg('登录成功', function(){
-                //do something
-                
-                    location.href='/home/index.html'
-                });
-            }
-        })
-    })
 })
+
+//=========================注册功能=========================
+$('#register').on('submit',function(e){
+    e.preventDefault()
+    let data=$(this).serialize()
+    // console.log(data);
+
+    // 判单
+    $.ajax({
+        url:'/api/reguser',
+        type:'POST',
+        data,
+        success:function(res){
+            console.log(res);
+            if (res.status !==0) {
+                return layer.msg(res.message)
+            }
+            layer.msg(res.message+'稍后自动跳转到登录页面',function(){
+                $('.register').hide()
+                $('.login').show()
+            })
+        }
+    })
+
+})
+
+
+//=========================表单验证=========================
+
+ // 注册页面表单验证规则
+
+ form.verify({
+     username: function(value, item){ //value：表单的值、item：表单的DOM对象
+       if(!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)){
+         return '用户名不能有特殊字符';
+       }
+       if(/(^\_)|(\__)|(\_+$)/.test(value)){
+         return '用户名首尾不能出现下划线\'_\'';
+       }
+       if(/^\d+\d+\d$/.test(value)){
+         return '用户名不能全为数字';
+       }
+     },
+
+    //  判断两次密码输入是否一致
+    verify:function(value,item){
+        // let one=$('#one').val()
+        // let two=$('#two').val()
+        // console.log(one,two);
+        if ( $('#one').val() !== $('#two').val()) {
+            return '两次密码输入不一致！'
+        }
+    },
+
+     //我们既支持上述函数式的方式，也支持下述数组的形式
+     //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
+     pass: [
+       /^[\S]{6,12}$/
+       ,'密码必须6到12位，且不能出现空格'
+     ] 
+   });  
